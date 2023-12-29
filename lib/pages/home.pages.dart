661a85aeacae025908aa32e0ecd7208e55699955
a-s-m-a-a <aa2025716@gmail.com>
2023-12-29 19:05:30 +0200

    task #6

--- lib/pages/home.pages.dart
+++ lib/pages/home.pages.dart
@@ -10,45 +10,69 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var sliderIndex =0;
+  var sliderIndex = 0;
+  CarouselController carController = CarouselController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Center(
             child: Column(children: [
-          CarouselSlider(
-            options: CarouselOptions(
-                autoPlay: true,
-                height: 200.0,
-                viewportFraction: .75,
-                enlargeCenterPage: true,
-                enlargeFactor: .3,
-                enlargeStrategy: CenterPageEnlargeStrategy.height,
-                onPageChanged: (index, _){
-                  sliderIndex = index;
-                }
-                ),
-                
-            items: [1, 2, 3, 4, 5].map((i) {
-              return Container(
-                  width: MediaQuery.of(context).size.width,
-                  margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                  decoration: const BoxDecoration(color: Colors.amber),
-                  child: Text(
-                    'text $i',
-                    style: const TextStyle(fontSize: 16.0),
-                  ));
-            }).toList(),
-          ),
+          Stack(children: [
+            CarouselSlider(
+              carouselController: carController,
+              options: CarouselOptions(
+                  autoPlay: true,
+                  height: 200.0,
+                  viewportFraction: .75,
+                  enlargeCenterPage: true,
+                  enlargeFactor: .3,
+                  enlargeStrategy: CenterPageEnlargeStrategy.height,
+                  onPageChanged: (index, _) {
+                    sliderIndex = index;
+                    setState(() {});
+                  }),
+              items: [1, 2, 3, 4, 5].map((i) {
+                return Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                    decoration: const BoxDecoration(color: Colors.amber),
+                    child: Text(
+                      'text $i',
+                      style: const TextStyle(fontSize: 16.0),
+                    ));
+              }).toList(),
+            ),
+            Positioned.fill(
+              child: Row(
+                children: [
+                  IconButton(
+                      onPressed: () async {
+                        await carController.previousPage();
+                        setState(() {});
+                      },
+                      icon: const Icon(Icons.arrow_back)),
+                  const Spacer(),
+                  IconButton(
+                      onPressed: () async {
+                        await carController.nextPage();
+                        setState(() {});
+                      },
+                      icon: const Icon(Icons.arrow_forward)),
+                ],
+              ),
+            )
+          ]),
           DotsIndicator(
             dotsCount: 5,
             position: sliderIndex,
             decorator: DotsDecorator(
               size: const Size.square(9.0),
               activeSize: const Size(18.0, 9.0),
-              activeShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
-            ),)
+              activeShape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0)),
+            ),
+          )
         ])),
       ),
     );
